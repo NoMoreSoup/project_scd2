f548902ba3908fa5dfe8b9b2c2bd0404bd961662
Egor Larin <evlarin95@gmail.com>
2026-06-13 19:35:12 +0300

    add DAG scd2_dag.py

--- dags/scd2_dag.py
+++ dags/scd2_dag.py
@@ -7,18 +7,19 @@ with DAG(
     start_date=datetime(2026, 1, 1),
     schedule_interval="*/1 * * * *",
     catchup=False,
+    template_searchpath="/opt/airflow/sql",
 ) as dag:
 
     ods_load = PostgresOperator(
         task_id="ods_load",
         postgres_conn_id="postgres_default",
-        sql="sql/ods_load.sql",
+        sql="ods_load.sql",
     )
 
     dds_load = PostgresOperator(
         task_id="dds_scd2_load",
         postgres_conn_id="postgres_default",
-        sql="sql/dds_scd2_load.sql",
+        sql="dds_scd2_load.sql",
     )
 
     ods_load >> dds_load
